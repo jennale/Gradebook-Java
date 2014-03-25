44afc0a0fe600bbb5d238e7da6acd6f914f9a6c2
Mikhail Lebedzeu <mlebedze@uwo.ca>
2014-03-25 15:20:33 -0400

    Unit test progress, checking with jenkins

--- src/test/java/cs2212/team4/CourseTest.java
+++ src/test/java/cs2212/team4/CourseTest.java
@@ -6,205 +6,109 @@ import org.junit.Test;
 
 public class CourseTest 
 {
-	Course crs, crs0, crs1, crs2, crs3, crs4, crs5, crs6, crs7, crs8, crs9;
+	Course course1, course2, course3, course4;
 	Student stud;
 	Deliverable deliver;
 	
 	@Before
 	public void testCourse()
 	{
-		crs=new Course("Title", "Term", "Code");
-		crs0=new Course("Computer Science", "A", "2208");
-		crs1=new Course("Computer Science", "A", "2210");
-		crs2=new Course("Computer Science", "A", "2211");
-		crs3=new Course("Computer Science", "A", "2214");
-		crs4=new Course("Statistical Sciences", "A", "2244");
-		crs5=new Course("Computer Science", "B", "2209");
-		crs6=new Course("Computer Science", "B", "2212");
-		crs7=new Course("Computer Science", "B", "3340");
-		crs8=new Course("Computer Science", "B", "3342");
-		crs9=new Course("Writing", "G", "2207");
+		course1=new Course("Different Title", "Different term", "Different code");
+		course2=new Course("Computer Science", "B", "2212");
+		course3=new Course("Computer Science", "B", "2209");
+		course4=new Course("Philosophy", "A", "2411");
 		
-		crs.addStudent("Zaid", "Albirawi", "250626000", "zalbiraw");
-		stud=new Student("Zaid", "Albirawi", "250626000", "zalbiraw");
+		course1.addStudent("Marry", "Poppins", "2509990505", "mpoppin9");
+		stud=new Student("Marry", "Poppins", "2509990505", "mpoppin9");
 		
-		crs.addDeliverable("Project", "Other", 54);
+		course1.addDeliverable("Project", "Other", 54);
 		deliver=new Deliverable("Project", "Other", 54, 0);
 		
-		crs.getStudent(0).addGrade(0, 100, "Other", 54);
+		course1.getStudent(0).addGrade(0, 100, "Other", 54);
 	}
 
 	@Test
 	public void testGetTitle()
 	{
-		Assert.assertTrue(crs0.getTitle().equals("Computer Science"));
-		Assert.assertTrue(crs1.getTitle().equals("Computer Science"));
-		Assert.assertTrue(crs2.getTitle().equals("Computer Science"));
-		Assert.assertTrue(crs3.getTitle().equals("Computer Science"));
-		Assert.assertFalse(crs4.getTitle().equals("Computer Science"));
-		Assert.assertTrue(crs5.getTitle().equals("Computer Science"));
-		Assert.assertTrue(crs6.getTitle().equals("Computer Science"));
-		Assert.assertTrue(crs7.getTitle().equals("Computer Science"));
-		Assert.assertTrue(crs8.getTitle().equals("Computer Science"));
-		Assert.assertFalse(crs9.getTitle().equals("Computer Science"));
+		Assert.assertTrue(course1.getTitle().equals("Different Title"));
+		Assert.assertTrue(course2.getTitle().equals("Computer Science"));
+		Assert.assertTrue(course3.getTitle().equals("Computer Science"));
+		Assert.assertTrue(course4.getTitle().equals("Philosophy"));
+		
+		Assert.assertFalse(course1.getTitle().equals("Computer Science"));
+		Assert.assertFalse(course2.getTitle().equals("other title"));
+		Assert.assertFalse(course3.getTitle().equals("wrong title"));
+		Assert.assertFalse(course4.getTitle().equals("hue"));
 	}
 
 	@Test
 	public void testGetTerm()
 	{
-		Assert.assertTrue(crs0.getTerm().equals("A"));
-		Assert.assertTrue(crs1.getTerm().equals("A"));
-		Assert.assertTrue(crs2.getTerm().equals("A"));
-		Assert.assertTrue(crs3.getTerm().equals("A"));
-		Assert.assertTrue(crs4.getTerm().equals("A"));
+		Assert.assertTrue(course1.getTerm().equals("Different term"));
+		Assert.assertTrue(course2.getTerm().equals("B"));
+		Assert.assertTrue(course3.getTerm().equals("B"));
+		Assert.assertTrue(course4.getTerm().equals("A"));
 		
-		Assert.assertFalse(crs5.getTerm().equals("A"));
-		Assert.assertFalse(crs6.getTerm().equals("A"));
-		Assert.assertFalse(crs7.getTerm().equals("A"));
-		Assert.assertFalse(crs8.getTerm().equals("A"));
-		Assert.assertFalse(crs9.getTerm().equals("A"));
+		Assert.assertFalse(course1.getTerm().equals("C"));
+		Assert.assertFalse(course2.getTerm().equals("C"));
+		Assert.assertFalse(course3.getTerm().equals("C"));
+		Assert.assertFalse(course4.getTerm().equals("other"));
 	}
 
 	@Test
 	public void testGetCode()
 	{
-		Assert.assertTrue(crs0.getCode().equals("2208"));
-		Assert.assertTrue(crs1.getCode().equals("2210"));
-		Assert.assertTrue(crs2.getCode().equals("2211"));
-		Assert.assertTrue(crs3.getCode().equals("2214"));
-		Assert.assertTrue(crs4.getCode().equals("2244"));
+		Assert.assertTrue(course1.getCode().equals("Different code"));
+		Assert.assertTrue(course2.getCode().equals("2212"));
+		Assert.assertTrue(course3.getCode().equals("2209"));
+		Assert.assertTrue(course4.getCode().equals("2411"));
 		
-		Assert.assertFalse(crs5.getCode().equals("2208"));
-		Assert.assertFalse(crs6.getCode().equals("2210"));
-		Assert.assertFalse(crs7.getCode().equals("2211"));
-		Assert.assertFalse(crs8.getCode().equals("2214"));
-		Assert.assertFalse(crs9.getCode().equals("2244"));
+		Assert.assertFalse(course1.getCode().equals("2208"));
+		Assert.assertFalse(course2.getCode().equals("2210"));
+		Assert.assertFalse(course3.getCode().equals("2211"));
+		Assert.assertFalse(course4.getCode().equals("2214"));
 	}
 
 	@Test
 	public void testGetStudent()
 	{
-		Assert.assertTrue(crs.getStudent(1)==null);
-		Assert.assertTrue(crs.getStudent(0).equals(stud));
+		Assert.assertTrue(course1.getStudent(1)==null);
+		Assert.assertTrue(course1.getStudent(0).equals(stud));
 	}
-
+	
 	@Test
 	public void testGetDeliverable()
 	{
-		Assert.assertTrue(crs.getDeliverable(1)==null);
-		Assert.assertTrue(crs.getDeliverable(0).equals(deliver));
+		Assert.assertTrue(course1.getDeliverable(1)==null);
+		Assert.assertTrue(course1.getDeliverable(0).equals(deliver));
 	}
 
 	@Test
 	public void testGetGrade()
 	{
-		Assert.assertTrue(crs.getGrade(crs.getStudent(0), 1)==-1);
-		Assert.assertTrue(crs.getGrade(crs.getStudent(0), 0)==100);
+		Assert.assertTrue(course1.getGrade(course1.getStudent(0), 1)==-1);
+		Assert.assertTrue(course1.getGrade(course1.getStudent(0), 0)==100);
 	}
-
+	
 	@Test
 	public void testSetTitle()
 	{
-		
+		course1.setTitle("New title");
+		Assert.assertTrue(course1.getTitle().equals("New title"));
 	}
-
-	@Test
-	public void testSetTerm() {
-
-	}
-
-	@Test
-	public void testSetCode() {
-
-	}
-
-	@Test
-	public void testEditStudentNumber() {
-
-	}
-
-	@Test
-	public void testEditStudentEmail() {
 	
-	}
-
 	@Test
-	public void testFindStudent() {
+	public void testSetTerm()
+	{
+		course1.setTerm("New term");
+		Assert.assertTrue(course1.getTerm().equals("New term"));
+	}
 	
-	}
-
 	@Test
-	public void testAddStudent() {
-		
+	public void testSetCode()
+	{
+		course1.setCode("New code");
+		Assert.assertTrue(course1.getCode().equals("New code"));
 	}
-
-	@Test
-	public void testRemoveStudent() {
-		
-	}
-
-	@Test
-	public void testFindDeliverable() {
-		
-	}
-
-	@Test
-	public void testAddDeliverable() {
 	
-	}
-
-	@Test
-	public void testRemoveDeliverable() {
-		
-	}
-
-	@Test
-	public void testAddGrade() {
-		
-	}
-
-	@Test
-	public void testRemoveGrade() {
-		
-	}
-
-	@Test
-	public void testImportStudents() {
-		
-	}
-
-	@Test
-	public void testExportStudents() {
-		
-	}
-
-	@Test
-	public void testImportDeliverables() {
-		
-	}
-
-	@Test
-	public void testExportDeliverables() {
-		
-	}
-
-	@Test
-	public void testImportGrades() {
-		
-	}
-
-	@Test
-	public void testExportGrades() {
-		
-	}
-
-	@Test
-	public void testEqualsCourse() {
-		
-	}
-
-	@Test
-	public void testToString() {
-		
-	}
 }
--- src/test/java/cs2212/team4/GradebookTest.java
+++ src/test/java/cs2212/team4/GradebookTest.java
@@ -16,89 +16,57 @@ import java.util.*;
 public class GradebookTest 
 {	
 	Gradebook gradebook = new Gradebook();
-	Course crs, crs0, crs1, crs2, crs3, crs4, crs5, crs6, crs7, crs8, crs9;
+	Course course1, course2, course3, course4;
 	
 	@Before
 	public void gradebook()
 	{
 		//Creates and adds Course objects to the Gradebook object.
-		crs=new Course("Title", "Term", "Code");
-		gradebook.addCourse("Computer Science", "A", "2208");
-		crs0=new Course("Computer Science", "A", "2208");
-		gradebook.addCourse("Computer Science", "A", "2210");
-		crs1=new Course("Computer Science", "A", "2210");
-		gradebook.addCourse("Computer Science", "A", "2211");
-		crs2=new Course("Computer Science", "A", "2211");
-		gradebook.addCourse("Computer Science", "A", "2214");
-		crs3=new Course("Computer Science", "A", "2214");
-		gradebook.addCourse("Statistical Sciences", "A", "2244");
-		crs4=new Course("Statistical Sciences", "A", "2244");
-		gradebook.addCourse("Computer Science", "B", "2209");
-		crs5=new Course("Computer Science", "B", "2209");
+		course1=new Course("Different Title", "Different Term", "Different Code");
+		
 		gradebook.addCourse("Computer Science", "B", "2212");
-		crs6=new Course("Computer Science", "B", "2212");
-		gradebook.addCourse("Computer Science", "B", "3340");
-		crs7=new Course("Computer Science", "B", "3340");
-		gradebook.addCourse("Computer Science", "B", "3342");
-		crs8=new Course("Computer Science", "B", "3342");
-		gradebook.addCourse("Writing", "G", "2207");
-		crs9=new Course("Writing", "G", "2207");
+		course2=new Course("Computer Science", "B", "2212");
+		
+		gradebook.addCourse("Computer Science", "B", "2209");
+		course3=new Course("Computer Science", "B", "2209");
+		
+		gradebook.addCourse("Philosophy", "A", "2411");
+		course4=new Course("Philosophy", "A", "2411");
 	}
 
 	@Test
 	public void testGetCourse()
 	{
-		Assert.assertTrue(crs0.equals(gradebook.getCourse(0)));
-		Assert.assertFalse(crs.equals(gradebook.getCourse(0)));
+		Assert.assertTrue(course2.equals(gradebook.getCourse(0)));
+		Assert.assertFalse(course1.equals(gradebook.getCourse(0)));
 		
-		Assert.assertTrue(crs1.equals(gradebook.getCourse(1)));
-		Assert.assertFalse(crs.equals(gradebook.getCourse(1)));
+		Assert.assertTrue(course3.equals(gradebook.getCourse(1)));
+		Assert.assertFalse(course1.equals(gradebook.getCourse(1)));
 		
-		Assert.assertTrue(crs2.equals(gradebook.getCourse(2)));
-		Assert.assertFalse(crs.equals(gradebook.getCourse(2)));
+		Assert.assertTrue(course4.equals(gradebook.getCourse(2)));
+		Assert.assertFalse(course1.equals(gradebook.getCourse(2)));
 		
-		Assert.assertTrue(crs3.equals(gradebook.getCourse(3)));
-		Assert.assertFalse(crs.equals(gradebook.getCourse(3)));
-		
-		Assert.assertTrue(crs4.equals(gradebook.getCourse(4)));
-		Assert.assertFalse(crs.equals(gradebook.getCourse(4)));
-		
-		Assert.assertTrue(crs5.equals(gradebook.getCourse(5)));
-		Assert.assertFalse(crs.equals(gradebook.getCourse(5)));
-		
-		Assert.assertTrue(crs6.equals(gradebook.getCourse(6)));
-		Assert.assertFalse(crs.equals(gradebook.getCourse(6)));
-		
-		Assert.assertTrue(crs7.equals(gradebook.getCourse(7)));
-		Assert.assertFalse(crs.equals(gradebook.getCourse(7)));
-		
-		Assert.assertTrue(crs8.equals(gradebook.getCourse(8)));
-		Assert.assertFalse(crs.equals(gradebook.getCourse(8)));
-		
-		Assert.assertTrue(crs9.equals(gradebook.getCourse(9)));
-		Assert.assertFalse(crs.equals(gradebook.getCourse(9)));
-		
-		Assert.assertTrue(gradebook.getCourse(10)==null);
+		Assert.assertTrue(gradebook.getCourse(3)==null);
 	}
-
+	
 	@Test
 	public void testGetPath()
 	{
 		Assert.assertTrue(gradebook.getPath().equals(""));
 	}
-
+	
 	@Test
 	public void testSetPath()
 	{
 		gradebook.setPath("New Path");
 		Assert.assertTrue(gradebook.getPath().equals("New Path"));
 	}
-
+	
 	@Test
 	public void testStore()
 	{
-		Assert.assertTrue(gradebook.store());
-		Assert.assertTrue(gradebook.store());
+		Assert.assertTrue(gradebook.store()==true);
+		Assert.assertTrue(gradebook.store()==true);
 	}
 	
 	@Test
@@ -107,39 +75,18 @@ public class GradebookTest
 		gradebook.store();
 		Gradebook gradebook2=new Gradebook();
 		
-		Assert.assertTrue(crs0.equals(gradebook2.getCourse(0)));
-		Assert.assertFalse(crs.equals(gradebook2.getCourse(0)));
+		Assert.assertTrue(course2.equals(gradebook2.getCourse(0)));
+		Assert.assertFalse(course1.equals(gradebook2.getCourse(0)));
 		
-		Assert.assertTrue(crs1.equals(gradebook2.getCourse(1)));
-		Assert.assertFalse(crs.equals(gradebook2.getCourse(1)));
+		Assert.assertTrue(course3.equals(gradebook2.getCourse(1)));
+		Assert.assertFalse(course1.equals(gradebook2.getCourse(1)));
 		
-		Assert.assertTrue(crs2.equals(gradebook2.getCourse(2)));
-		Assert.assertFalse(crs.equals(gradebook2.getCourse(2)));
+		Assert.assertTrue(course4.equals(gradebook2.getCourse(2)));
+		Assert.assertFalse(course1.equals(gradebook2.getCourse(2)));
 		
-		Assert.assertTrue(crs3.equals(gradebook2.getCourse(3)));
-		Assert.assertFalse(crs.equals(gradebook2.getCourse(3)));
-		
-		Assert.assertTrue(crs4.equals(gradebook2.getCourse(4)));
-		Assert.assertFalse(crs.equals(gradebook2.getCourse(4)));
-		
-		Assert.assertTrue(crs5.equals(gradebook2.getCourse(5)));
-		Assert.assertFalse(crs.equals(gradebook2.getCourse(5)));
-		
-		Assert.assertTrue(crs6.equals(gradebook2.getCourse(6)));
-		Assert.assertFalse(crs.equals(gradebook2.getCourse(6)));
-		
-		Assert.assertTrue(crs7.equals(gradebook2.getCourse(7)));
-		Assert.assertFalse(crs.equals(gradebook2.getCourse(7)));
-		
-		Assert.assertTrue(crs8.equals(gradebook2.getCourse(8)));
-		Assert.assertFalse(crs.equals(gradebook2.getCourse(8)));
-		
-		Assert.assertTrue(crs9.equals(gradebook2.getCourse(9)));
-		Assert.assertFalse(crs.equals(gradebook2.getCourse(9)));
-		
-		Assert.assertTrue(gradebook2.getCourse(10)==null);
+		Assert.assertTrue(gradebook.getCourse(3)==null);
 	}
-
+	
 	@Test
 	public void testAddCourse()
 	{
@@ -150,22 +97,18 @@ public class GradebookTest
 		Assert.assertTrue(gradebook.addCourse("Title3", "Term3", "Code3"));
 		Assert.assertTrue(gradebook.addCourse("Title4", "Term4", "Code4"));
 		//Add existent Course objects to the Gradebook object.
-		Assert.assertFalse(gradebook.addCourse("Computer Science", "B", "2209"));
 		Assert.assertFalse(gradebook.addCourse("Computer Science", "B", "2212"));
-		Assert.assertFalse(gradebook.addCourse("Computer Science", "B", "3340"));
-		Assert.assertFalse(gradebook.addCourse("Computer Science", "B", "3342"));
-		Assert.assertFalse(gradebook.addCourse("Writing", "G", "2207"));
+		Assert.assertFalse(gradebook.addCourse("Computer Science", "B", "2209"));
+		Assert.assertFalse(gradebook.addCourse("Philosophy", "A", "2411"));
 	}
-
+	
 	@Test
 	public void testRemoveCourse()
 	{
 		//Remove existent Course objects to the Gradebook object.
-		Assert.assertTrue(gradebook.removeCourse(crs0));
-		Assert.assertTrue(gradebook.removeCourse(crs1));
-		Assert.assertTrue(gradebook.removeCourse(crs2));
-		Assert.assertTrue(gradebook.removeCourse(crs3));
-		Assert.assertTrue(gradebook.removeCourse(crs4));
+		Assert.assertTrue(gradebook.removeCourse(course2));
+		Assert.assertTrue(gradebook.removeCourse(course3));
+		Assert.assertTrue(gradebook.removeCourse(course4));
 		//Remove none-existent Course objects to the Gradebook object.
 		Assert.assertFalse(gradebook.removeCourse(new Course ("Title0", "Term0", "Code0")));
 		Assert.assertFalse(gradebook.removeCourse(new Course ("Title1", "Term1", "Code1")));

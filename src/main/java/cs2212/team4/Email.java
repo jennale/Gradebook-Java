ceafdc5886aac44538ac016af6e556998938fb70
Zaid Albirawi <zalbiraw@uwo.ca>
2014-03-29 23:39:25 -0400

    Emailing and Reports

---
+++ src/main/java/cs2212/team4/Email.java
@@ -0,0 +1,131 @@
+package cs2212.team4;
+
+import java.io.*;
+import java.net.URL;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.URLDataSource;
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.*;
+
+public class Email {
+
+	static String msgSubject;
+	static Properties properties;
+	static String msg;
+	static Student student;
+	static Course course;
+	static boolean boolReport;
+
+	public Email(Course course, Student student, String msgSubject, String msg,
+			boolean boolReport) {
+		Email.msgSubject = msgSubject;
+		Email.msg = msg;
+		Email.student = student;
+		Email.course = course;
+		Email.boolReport = boolReport;
+	}
+
+	private static Session getSession(final Properties properties) {
+		Session session = Session.getInstance(properties,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						String username = properties
+								.getProperty("smtp.username");
+						String password = properties
+								.getProperty("smtp.password");
+						return new PasswordAuthentication(username, password);
+					}
+				});
+		return session;
+	}
+
+	private static String loadProperties() {
+		Properties properties = new Properties();
+		try {
+			InputStream stream = GradebookGUI.class.getClassLoader()
+					.getResourceAsStream("cs2212/team4/mail.properties");
+			if (stream == null)
+				return "Fail to import mail.properties";
+			properties.load(stream);
+		} catch (IOException e) {
+			return "Fail to import mail.properties";
+		}
+		Email.properties = properties;
+		return "";
+	}
+
+	private static String sendMessage(Session session, Properties properties,
+			String studentEmail) {
+		try {
+			String returnMsg;
+
+			Message msg = new MimeMessage(session);
+			String senderName = properties.getProperty("sender.name");
+			String senderEmail = properties.getProperty("sender.email");
+
+			Address sender;
+			sender = new InternetAddress(senderEmail, senderName);
+
+			msg.setFrom(sender);
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(
+					studentEmail));
+			msg.setSubject(msgSubject);
+
+			Multipart multiPart = new MimeMultipart();
+
+			MimeBodyPart textPart = new MimeBodyPart();
+			textPart.setText(Email.msg);
+			multiPart.addBodyPart(textPart);
+			
+			returnMsg = generateReport();
+
+			if (Email.boolReport){
+				if (!returnMsg.equals(""))
+					return returnMsg;
+
+				MimeBodyPart fileAttachmentPart = new MimeBodyPart();
+				URL attachmentUrl = GradebookGUI.class.getClassLoader()
+						.getResource("cs2212/team4/report.pdf");
+				if (attachmentUrl == null)
+					return "Failed a generate an attachment url";
+				DataSource source = new URLDataSource(attachmentUrl);
+				fileAttachmentPart.setDataHandler(new DataHandler(source));
+				fileAttachmentPart.setFileName("report.pdf");
+
+				multiPart.addBodyPart(fileAttachmentPart);
+			}
+
+			msg.setContent(multiPart);
+			Transport.send(msg);
+		} catch (UnsupportedEncodingException e) {
+			return "Failed to create a new sender internet address";
+		} catch (MessagingException e) {
+			return "Failed to create a message";
+		}
+		return "";
+	}
+
+	private static String generateReport() {
+		String returnMsg;
+		Report report = new Report(course, student);
+		if (!(returnMsg = report.generateReport()).equals(""))
+			return returnMsg;
+		return "";
+	}
+
+	public String sendEmail() {
+		String returnMsg;
+		Properties properties;
+
+		if (!(returnMsg = loadProperties()).equals(""))
+			return returnMsg;
+		properties = Email.properties;
+		Session session = getSession(properties);
+		if (!(returnMsg = sendMessage(session, properties, student.getEmail()))
+				.equals(""))
+			return returnMsg;
+		return "";
+	}
+}
--- src/main/java/cs2212/team4/Report.java
+++ src/main/java/cs2212/team4/Report.java
@@ -19,29 +19,13 @@ import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
 
 public class Report {
-	public Report(Student student, Course course) {
-		try {
-			InputStream logoStream = loadResource("cs2212/team4/logo.png");
-			InputStream reportStream = loadResource("cs2212/team4/report.jrxml");
-			BufferedImage logo = ImageIO.read(logoStream);
-			Collection<StudentReportData> customers = reportData(student,
-					course);
-			JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(
-					customers);
-			Map<String, Object> parameters = new HashMap<String, Object>();
-			parameters.put("logo", logo);
-			JasperDesign jasperDesign = JRXmlLoader.load(reportStream);
-			JasperReport jasperReport = JasperCompileManager
-					.compileReport(jasperDesign);
-			JasperPrint jasperPrint = JasperFillManager.fillReport(
-					jasperReport, parameters, beanColDataSource);
-			JasperExportManager
-					.exportReportToPdfFile(jasperPrint, "report.pdf");
-		} catch (JRException e) {
-			System.out.println(e);
-		} catch (IOException e) {
-			System.out.println(e);
-		}
+
+	private Course course;
+	private Student student;
+
+	public Report(Course course, Student student) {
+		this.course = course;
+		this.student = student;
 	}
 
 	private static InputStream loadResource(String filename) {
@@ -54,10 +38,40 @@ public class Report {
 		Collection<StudentReportData> temp = new ArrayList<StudentReportData>();
 		temp.add(new StudentReportData(student, course, -1));
 		for (int i = 0; i < course.getDeliverableListSize(); i++) {
-			if (course.getDeliverable(i)!=null){
+			if (course.getDeliverable(i) != null) {
 				temp.add(new StudentReportData(student, course, i));
 			}
 		}
 		return temp;
+	}
+
+	public String generateReport() {
+		try {
+			InputStream logoStream = loadResource("cs2212/team4/logo.png");
+			if (logoStream == null)
+				return "Cannot find logo.png";
+			InputStream reportStream = loadResource("cs2212/team4/report.jrxml");
+			if (reportStream == null)
+				return "Cannot find report.jrxml";
+			BufferedImage logo = ImageIO.read(logoStream);
+			Collection<StudentReportData> reportData = reportData(student,
+					course);
+			JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(
+					reportData);
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			parameters.put("logo", logo);
+			JasperDesign jasperDesign = JRXmlLoader.load(reportStream);
+			JasperReport jasperReport = JasperCompileManager
+					.compileReport(jasperDesign);
+			JasperPrint jasperPrint = JasperFillManager.fillReport(
+					jasperReport, parameters, beanColDataSource);
+			JasperExportManager.exportReportToPdfFile(jasperPrint,
+					"src/main/resources/cs2212/team4/report.pdf");
+		} catch (JRException e) {
+			return "File is non-writable";
+		} catch (IOException e) {
+			return "Gradebook was unable to import required files";
+		}
+		return "";
 	}
 }

e915c924810e8b39f64cff6e07c18f8cff117a86
Steve Juarez <sjuarez3@uwo.ca>
2014-04-01 12:53:54 -0400

    small tweak

--- src/main/java/cs2212/team4/Gradebook.java
+++ src/main/java/cs2212/team4/Gradebook.java
@@ -51,7 +51,7 @@ public class Gradebook implements GradebookADT, Serializable
 	 * @return    the course at the specified index
 	 */
 	public Course getCourse(int crs) {
-		if (courseList.size() > crs)
+		if (courseList.size() > crs && crs >= 0)
 			return courseList.get(crs);
 		return null;
 	}

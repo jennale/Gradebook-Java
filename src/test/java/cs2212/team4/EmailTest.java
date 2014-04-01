2cedec2ad0ad44f7ef84058af5f15e8e7829545e
Justin Cheng <jchen528@uwo.ca>
2014-04-01 00:29:48 -0400

    email test begins

---
+++ src/test/java/cs2212/team4/EmailTest.java
@@ -0,0 +1,44 @@
+package cs2212.team4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class EmailTest {
+	
+	@Before
+	public void testEmail () {
+	
+	}
+	
+	@Test
+	public void testgetSession ()
+	{
+	
+	}
+	
+	@Test
+	public void testloadProperties ()
+	{
+	
+	}
+	
+	@Test
+	public void testsendMessage ()
+	{
+	
+	}
+	
+	@Test
+	public void testgenerateReport ()
+	{
+	
+	}
+	
+	@Test
+	public void testsendEmail ()
+	{
+	
+	}
+}

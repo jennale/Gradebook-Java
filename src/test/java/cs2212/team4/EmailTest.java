01dcfa043c9077be03804ae7af18231b536bc549
Justin Cheng <jchen528@uwo.ca>
2014-04-01 03:11:19 -0400

    studentreportdatatest updates

--- src/test/java/cs2212/team4/EmailTest.java
+++ src/test/java/cs2212/team4/EmailTest.java
@@ -1,15 +1,30 @@
 package cs2212.team4;
 
-import static org.junit.Assert.*;
-
+import junit.framework.Assert;
+import java.util.*;
 import org.junit.Test;
 import org.junit.Before;
 
 public class EmailTest {
 	
-	@Before
-	public void testEmail () {
 	
+	Course course1, course2;
+	Student stud1, stud2;
+	Email email1, email2;
+	Properties prop1, prop2;
+	
+	@Before
+	public void createProperties (){
+		
+	}
+	
+	@Before
+	public void testEmail ()
+	{
+		course1 = new Course( "Satanism 101", "A", "2304");
+		stud1 = new Student( "Mel", "Gibson", "250544365", "melgibs22@uwo.ca");
+		email1 = new Email( course1, stud1, "Mel Gibson's Grades", "Here are your grades good sir", true);
+		
 	}
 	
 	@Test

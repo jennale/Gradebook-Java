1e938495a802140f28895d4c672cec115bce2fcc
Steve Juarez <sjuarez3@uwo.ca>
2014-04-02 16:16:46 -0400

    input JavaDoc

--- src/main/java/cs2212/team4/MyProperties.java
+++ src/main/java/cs2212/team4/MyProperties.java
@@ -2,10 +2,21 @@ package cs2212.team4;
 
 import java.util.Properties;
 
+/**
+ * This class allows the Email class to access multiple mailing services.
+ */
+
 public class MyProperties {
 
 	private Properties properties;
 
+	/**
+	 * Constructor to setup a property with specified parameters.
+	 * 
+	 * @param username the property's username
+	 * @param password the property's password
+	 * @param provider the property's email service provider
+	 */
 	public MyProperties(String username, String password, String provider) {
 		properties = new Properties();
 		
@@ -27,6 +38,15 @@ public class MyProperties {
 		properties.put("smtp.password", password);
 	}
 	
+	/**
+	 * Constructor to setup property with specified parameters.
+	 * 
+	 * @param mailHost   the property's mail host
+	 * @param socketPort the property's socket port
+	 * @param port       the property's port
+	 * @param username   the property's username
+	 * @param password   the property's password
+	 */
 	public MyProperties(String mailHost, String socketPort, String port,
 			String username, String password) {
 		properties = new Properties();
@@ -42,6 +62,11 @@ public class MyProperties {
 		properties.put("smtp.password", password);
 	}
 
+	/**
+	 * Gets the property.
+	 * 
+	 * @return properties the properties
+	 */
 	public Properties getProperties() {
 		return properties;
 	}

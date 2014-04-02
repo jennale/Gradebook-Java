546203190a6cdeef61cf67fe1cb101576441abe3
Justin Cheng <jchen528@uwo.ca>
2014-04-02 14:39:13 -0400

    updated myavgtest

--- src/test/java/cs2212/team4/MyAvgTest.java
+++ src/test/java/cs2212/team4/MyAvgTest.java
@@ -17,14 +17,17 @@ public class MyAvgTest {
 		course1.addStudent("Barney", "Stinson", "987654321", "barneystin@uwo.ca");
 		course1.addStudent("Steve", "Rogers", "123456789", "steverogers@uwo.ca");
 		
-		course1.getStudent(0).addGrade(0, 91, "assignment", 50);
+		course1.addDeliverable("Project", "assignment", 40);
+		course1.addDeliverable("Final", "exam", 50);
+		
+		course1.getStudent(0).addGrade(0, 71, "assignment", 40);
 		course1.getStudent(0).addGrade(1, 63, "exam", 50);
 		
-		course1.getStudent(1).addGrade(0, 77, "assignment", 50);
+		course1.getStudent(1).addGrade(0, 77, "assignment", 40);
 		course1.getStudent(1).addGrade(1, 88, "exam", 50);
 		
-		course1.getStudent(2).addGrade(0, 55, "assignment", 50);
-		course1.getStudent(2).addGrade(1, 99, "exam", 50);
+		course1.getStudent(2).addGrade(0, 55, "assignment", 40);
+		course1.getStudent(2).addGrade(1, 62, "exam", 50);
 		
 		myAvg1 = new MyAvg( course1, course1.getStudent(0) );
 		myAvg2 = new MyAvg( course1, course1.getStudent(1) );
@@ -33,9 +36,11 @@ public class MyAvgTest {
 
 	@Test
 	public void testCalcAvg() {
-		//Assert.assertEquals( 77.0, myAvg1);
-		//Assert.assertEquals( 82.5, myAvg2);
-		//Assert.assertEquals( 77.0, myAvg3);
+		System.out.println( "HERROOOOOOOOOOOOOOOOOOOOOO " + myAvg1.calcAvg(68.0) );
+		Assert.assertTrue( myAvg1.calcAvg(68.0) == 81.00000000000003);
+		
+		//Assert.assertEquals( myAvg2.calcAvg(80.0) );
+		//Assert.assertEquals( myAvg3.calcAvg(70.0) );
 	}
 
 }
